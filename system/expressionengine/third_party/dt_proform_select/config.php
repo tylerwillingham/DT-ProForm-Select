8fed957d0ad32e4b9efcf213f12f6201ca793a6b
Jeff Claeson <jeff@bydreamtime.com>
2011-11-30 10:05:39 -0800

    Initial Commit

---
+++ system/expressionengine/third_party/dt_proform_select/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+* DT ProForm Select config file
+*
+* @package		dt_proform_select
+* @author		Dreamtime ~ Jeff Claeson <jeff@bydreamtime.com>
+* @link			https://github.com/dreamtime/taf_freeform_templates
+* @copyright	Copyright 2011, Dreamtime
+*/
+
+if ( ! defined('DT_PF_NAME'))
+{
+	define('DT_PF_NAME',         'DT Proform Select');
+	define('DT_PF_VERSION',      '1.0');
+	define('DT_PF_DOCS',         'http://github.com/dreamtime/dt_proform_select');
+}
+
+$config['name']    = DT_PF_NAME;
+$config['version'] = DT_PF_VERSION;
+
+/* End of file ft.dt_proform_select.php */
+/* Location: ./system/expressionengine/third_party/dt_proform_select/ft.dt_proform_select.php */
---
+++ system/expressionengine/third_party/dt_proform_select/ft.dt_proform_select.php
@@ -0,0 +1,111 @@
+<?php if ( ! defined('EXT')) exit('Invalid file request');
+
+// Get config file
+require(PATH_THIRD.'dt_proform_select/config.php');
+
+
+/**
+* DT ProForm Select config file
+*
+* @version		1.0
+* @package		dt_proform_select
+* @author		Dreamtime ~ Jeff Claeson <jeff@bydreamtime.com>
+* @copyright	Copyright 2011, Dreamtime
+*/
+class Dt_proform_select_ft extends EE_Fieldtype {
+
+	/**
+	* Info array
+	*
+	* @var	array
+	*/
+	var $info = array(
+		'name'		=> DT_PF_NAME,
+		'version'	=> DT_PF_VERSION
+	);
+
+	// --------------------------------------------------------------------
+
+	/**
+	* PHP4 Constructor
+	*
+	* @see	__construct()
+	*/
+	function Dt_proform_select_ft()
+	{
+		$this->__construct();
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	* PHP5 Constructor
+	*
+	* @return	void
+	*/
+	function __construct()
+	{
+		parent::EE_Fieldtype();
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	* Displays the field in publish form
+	*
+	* @param	string
+	* @param	bool
+	* @return	string
+	*/
+	function display_field($data, $cell = FALSE)
+	{
+		// Load helper
+		$this->EE->load->helper('form');
+
+		// Get fields from DB
+		$query = $this->EE->db->query("SELECT form_label AS label, form_name AS name FROM exp_proform_forms ORDER BY name ASC");
+
+		// Generate drop down
+		$options = array('' => 'Please Select...');
+		foreach ($query->result_array() AS $row)
+		{
+			$options[$row['name']] = $row['label'];
+		}
+
+		// Field name depending on Matrix cell or not
+		$field_name = $cell ? $this->cell_name : $this->field_name;
+
+		return form_dropdown($field_name, $options, $data);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	* Displays the field in matrix
+	*
+	* @param	string
+	* @return	string
+	*/
+	function display_cell($cell_data)
+	{	
+		return $this->display_field($cell_data, TRUE);
+	}
+	
+	// --------------------------------------------------------------------
+	
+	/**
+	* Displays the field in Low Variables
+	*
+	* @param	string
+	* @return	string
+	*/
+	function display_var_field($var_data)
+	{
+		return $this->display_field($var_data);
+	}
+
+	// --------------------------------------------------------------------
+
+}
+
+/* End of file ft.dt_proform_select.php */
